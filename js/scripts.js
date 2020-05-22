74e403320af7055294c8e0a1d5904cc2d611e34e
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-22 03:20:12 -0300

    bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -80,67 +80,12 @@ jQuery(document).ready(function($) {
 			}
 		});
 
-//Twitter
-  $('.tweets-list-container').tweetscroll({ 
-    username: 'envatowebdesign', 
-    time: true, 
-    limit: 11,
-    replies: true, 
-    position: 'append', 
-    date_format: 'style2', 
-    animation: 'slide_up', 
-    visible_tweets: 1 
-  });
 
 
-//tooltips
-    $(document).ready(function ()
-    { 
-      $( 'body' ).tooltip({
-        selector: "a[data-toggle=tooltip]"
-      });
- });
-//Animated Progress Bars
-	$('.skill li').each(function () {
-		$(this).appear(function() {
-		  $(this).animate({opacity:1,left:"0px"},1200);
-		  var b = $(this).find("span").attr("data-width");
-		  $(this).find("span").animate({
-			width: b + "%"
-		  }, 1700, "easeOutCirc");
-		});	
-	});
 
-//Animated Counters
-	$('.count').each(function () {
-		$(".total-numbers .sum").appear(function() {
-		var counter = $(this).html();
-		$(this).countTo({
-			from: 0,
-			to: counter,
-			speed: 2000,
-			refreshInterval: 60,
-			});
-		});
-	});
+
 
 			 				
-
-//Responsive slide For blog
-	$("#blog-carousel").owlCarousel({
-		autoPlay: 3000,
-		navigation : false, // Show next and prev buttons
-		slideSpeed : 300,
-		paginationSpeed : 400,
-		singleItem:true
-									 
-		// "singleItem:true" is a shortcut for:
-		// items : 1,
-		// itemsDesktop : false,
-		// itemsDesktopSmall : false,
-		// itemsTablet: false,
-		// itemsMobile : false
-	});
 
 //Portfolio Isotop
 $(window).load(function() {   
@@ -205,62 +150,8 @@ $(window).load(function() {
 
 
 
-
- 	//Google Maps
-      $('#gmap').gmap3({
-        marker:{address:"Haltern am See, Weseler Str. 151", options:{icon: "img/location1.png"}},
-        map:{
-            options:{
-              zoom: 14,
-			  scrollwheel: false
-            }
-           }
-      });
-
 				
-     //Contact From
-    $(document).ready(function ()
-    { 
-     $('#submit').formValidator({
-        scope: '#form'
-      });
-      
-      $('#post-commentsss').formValidator({
-        scope: '#comments-form'
-      });
-      
-      $('#submit,#post-commentsss').click(function() {
-            $('input.error-input, textarea.error-input').delay(300).animate({marginLeft:0},100).animate({marginLeft:10},100).animate({marginLeft:0},100).animate({marginLeft:10},100);
-        });
-
-      // Form plugin
-
-      var options = {
-
-        beforeSubmit: function() {
-          $('.sending').show();
-
-        },
-        success: function() {
-          $('.sending').hide();
-          $('#form').hide();
-          $(".mess").show().html('<h5>Thanks !</h5><h5>Your message has been sent.</h5>'); // Change Your message post send
-          $('.mess').delay(3000).fadeOut(function() {
-
-            $('#form').clearForm();
-            $('#form').delay(3500).show();
-
-          });
-        }
-      };
-      
-
-      $('#form').submit(function() {
-        $(this).ajaxSubmit(options);
-        return false;
-      });
-        
-    });      
+     
 
 });	
 
