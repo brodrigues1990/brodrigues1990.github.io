74952c912f934c909ac989ebfd50367b5c477735
Bruno Rodrigues <brodrigues1990@gmail.com>
2020-05-22 03:27:48 -0300

    bugfix goto smoth

--- js/smoothScroll-ES5.js
+++ js/smoothScroll-ES5.js
@@ -1,50 +1,52 @@
-window.onload = function() {
-
-const easeInCubic = function (t) { return t*t*t }   
-const scrollElems = document.getElementsByClassName('scroll');
-
-
-//console.log(scrollElems);
-const scrollToElem = (start, stamp, duration, scrollEndElemTop, startScrollOffset) => {
-    //debugger;
-    const runtime = stamp - start;
-    let progress = runtime / duration;
-    const ease = easeInCubic(progress);
-    
-    progress = Math.min(progress, 1);
-    console.log(startScrollOffset,startScrollOffset + (scrollEndElemTop * ease));
-    
-    const newScrollOffset = startScrollOffset + (scrollEndElemTop * ease);
-    window.scroll(0, startScrollOffset + (scrollEndElemTop * ease));
-
-    if(runtime < duration){
-      requestAnimationFrame((timestamp) => {
-        const stamp = new Date().getTime();
-        scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
-      })
+// smooth scroll
+(function() {
+    'use strict';
+    // Feature Test
+    if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+        // Function to animate the scroll
+        var smoothScroll = function (anchor, duration) {
+            // Calculate how far and how fast to scroll
+            var startLocation = window.pageYOffset;
+            var endLocation = anchor.offsetTop;
+            var distance = endLocation - startLocation;
+            var increments = distance/(duration/16);
+            var stopAnimation;
+            // Scroll the page by an increment, and check if it's time to stop
+            var animateScroll = function () {
+                window.scrollBy(0, increments);
+                stopAnimation();
+            };
+            // If scrolling down
+            if ( increments >= 0 ) {
+                // Stop animation when you reach the anchor OR the bottom of the page
+                stopAnimation = function () {
+                    var travelled = window.pageYOffset;
+                    if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
+                        clearInterval(runAnimation);
+                    }
+                };
+            }
+            // Loop the animation function
+            var runAnimation = setInterval(animateScroll, 16);
+        };
+        // Define smooth scroll links
+        var scrollToggle = document.querySelectorAll('.scroll');
+        // For each smooth scroll link
+        [].forEach.call(scrollToggle, function (toggle) {
+            // When the smooth scroll link is clicked
+            toggle.addEventListener('click', function(e) {
+                // Prevent the default link behavior
+                e.preventDefault();
+                // Get anchor link and calculate distance from the top
+                var dataID = toggle.getAttribute('href');
+                var dataTarget = document.querySelector(dataID);
+                var dataSpeed = toggle.getAttribute('data-speed');
+                // If the anchor exists
+                if (dataTarget) {
+                    // Scroll to the anchor
+                    smoothScroll(dataTarget, dataSpeed || 500);
+                }
+            }, false);
+        });
     }
-  }
-
-for(let i=0; i<scrollElems.length; i++){
-  const elem = scrollElems[i];
-  
-  elem.addEventListener('click',function(e) {
-    e.preventDefault();
-    const scrollElemId = e.target.href.split('#')[1];
-    const scrollEndElem = document.getElementById(scrollElemId);
-    
-    const anim = requestAnimationFrame(() => {
-      const stamp = new Date().getTime();
-      const duration = 1200;
-      const start = stamp;
-          
-      const startScrollOffset = window.pageYOffset;
-
-      const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top;
-            
-      scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset);
-      // scrollToElem(scrollEndElemTop);
-      })
-    })
-  }
-}
+})();
